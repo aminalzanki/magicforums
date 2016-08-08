16586e50e971deb2827fa7e6b696e4f4b8cdc1c4
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-08-08 14:44:02 +0800

    fix create new commment

--- app/helpers/meta_path_helper.rb
+++ app/helpers/meta_path_helper.rb
@@ -7,6 +7,8 @@ module MetaPathHelper
       link_to "New Topic", new_topic_path
     when "posts#index"
       link_to "New Post", new_topic_post_path(@topic)
+    when "comments#index"
+      link_to "New Comment", new_topic_post_comment_path
     else
       link_to "Topics", topics_path
     end
