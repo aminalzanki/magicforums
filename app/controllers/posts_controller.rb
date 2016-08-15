6cd279e15eccdf9117059a0eb1ab0ed81db13a83
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-08-15 15:48:03 +0800

    ajax update for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,60 @@
 class PostsController < ApplicationController
 respond_to :js
 before_action :authenticate!, only: [:create, :edit, :update, :new, :destroy]
-def index
-  @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
-  @posts = @topic.posts.order("created_at ASC")
-  @post = Post.new
-end
-
-# def new
-#   @topic = Topic.find_by(id: params[:topic_id])
-#   @post = Post.new
-# end
-
-def create
-  @topic = Topic.find_by(id: params[:topic_id])
-  @post = current_user.posts.build(post_params.merge(topic_id: params[:topic_id]))
-
-  if @post.save
-    redirect_to topic_posts_path(@topic)
-  else
-    redirect_to new_topic_post_path(@topic)
+  def index
+    @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
+    @posts = @topic.posts.order("created_at ASC")
+    @post = Post.new
   end
-end
 
-def edit
-  @post = Post.find_by(id: params[:id])
-  @topic = @post.topic
-  authorize @post
-end
+  # def new
+  #   @topic = Topic.find_by(id: params[:topic_id])
+  #   @post = Post.new
+  # end
 
-def update
-  @post = Post.find_by(id: params[:id])
-  @topic = @post.topic
-  authorize @post
+  def create
+    @topic = Topic.find_by(id: params[:topic_id])
+    @post = current_user.posts.build(post_params.merge(topic_id: params[:topic_id]))
 
-  if @post.update(post_params)
-    redirect_to topic_posts_path(@topic)
-  else
-    redirect_to edit_topic_post_path(@topic, @post)
+    if @post.save
+      redirect_to topic_posts_path(@topic)
+    else
+      redirect_to new_topic_post_path(@topic)
+    end
   end
-end
 
-def destroy
-  @post = Post.find_by(id: params[:id])
-  @topic = @post.topic
-
-  if @post.destroy
-    redirect_to topic_posts_path(@topic)
+  def edit
+    @post = Post.find_by(id: params[:id])
+    @topic = @post.topic
+    authorize @post
   end
-end
 
-private
+  def update
+    @post = Post.find_by(id: params[:id])
+    @topic = @post.topic
+    authorize @post
 
-def post_params
-  params.require(:post).permit(:title, :body, :image)
-end
+    if @post.update(post_params)
+      # redirect_to topic_posts_path(@topic)
+      flash.now[:success] = "Post updated"
+    else
+      # redirect_to edit_topic_post_path(@topic, @post)
+      flash.now[:danger] = @comment.errors.full_messages
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @topic = @post.topic
+
+    if @post.destroy
+      redirect_to topic_posts_path(@topic)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
